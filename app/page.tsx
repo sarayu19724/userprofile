7bc92111ccb82e840031f6ef1bb3bc31f3ebf498
sarayu19724 <madigundusarayu@gmail.com>
2024-07-31 22:55:58 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+const HomePage=() => {
+  return(
+    <>
+     <div className="side">
+      <img src="images" />
+      
+      <svg dataSlot="icon" fill="none" strokeWidth={1.5} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="30px" height="30px">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+      </svg>
+      <h6>Profile</h6>
+      <svg dataSlot="icon" fill="none" strokeWidth={1.5} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="30px" height="30px">
+       <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+       </svg>
+      <h1>My Dashboard</h1>
+      <svg dataSlot="icon" fill="none" strokeWidth={1.5} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="30px" height="30px">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+      </svg>
+      <h2>Wishlist</h2>
+      <svg dataSlot="icon" fill="none" strokeWidth={1.5} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="30px"  height="30px" >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+      </svg>
+      <h4>completed projects</h4>
+      <svg dataSlot="icon" fill="none" strokeWidth={1.5} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="30px"  height="30px">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
+      </svg>
+      <h5>Complaints</h5>
+      
+      
+    </div>
+    <div className="main">
+    <svg dataSlot="icon" fill="none" strokeWidth={1.5} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="30px"  height="30px">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+    </svg>
+     <img src="images" />
+     <div className="name">
+        <h1>SAMMUEL <div className="sub"> Frontend Developer</div></h1>
+      </div>
+     <h1>About</h1>
+     <h2>I served as the Frontend Developer in our team for the “Eco-Friendly Travel Companion” project.I served as the Frontend Developer in our team for the “Eco-Friendly Travel Companion” project.I created wireframes and mockups for the app, ensuring an intuitive and visually appealing interface. I collaborated closely with the UX designer to implement user-friendly features.
+     Using HTML, CSS, and JavaScript, I built the frontend components. I optimized the app for various devices, ensuring seamless user experiences on both desktop and mobile.
+     </h2>
+     
+     <div className="b">
+       <h1>Basic Information</h1>
+       <h2>Age <div className="sub">
+        28 years</div></h2>
+        <h3>Years of Experience <div className="sub">
+        6 years</div></h3>
+        <h4>Phone <div className="sub">
+        xxxxx</div></h4>
+        <h5>Ctc <div className="sub">
+        12.5 lac</div></h5>
+        <h6>Email <div className="sub">
+        hhh@gmail.com</div></h6>
+        <div className="box">
+           <h1>Edit Profile</h1>
+        </div>
+     </div>
+     <div className="s">
+      <h1>Experience</h1>
+      <svg dataSlot="icon" fill="none" strokeWidth={1.5} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="30px"  height="30px">
+      <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+      </svg>
+     </div>
+     <div className="p">
+      <h1>Education</h1>
+      <svg dataSlot="icon" fill="none" strokeWidth={1.5} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="30px"  height="30px">
+      <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+      </svg>
+     </div>
+     <div className="r">
+      <h1>Accomplishments</h1>
+      <svg dataSlot="icon" fill="none" strokeWidth={1.5} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="30px"  height="30px">
+      <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+      </svg>
+     </div>
+
+    </div>
+    </>
+    
+    
+  );
+};
+export default HomePage;
